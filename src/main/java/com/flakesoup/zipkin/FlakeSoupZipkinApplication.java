06ac55a609b3c1b5aeb843402e1a5de66575b38f
LiJinsong <lijinsong@zhixuehd.com>
2019-11-06 11:19:16 +0800

    init
    Signed-off-by: LiJinsong <lijinsong@zhixuehd.com>

---
+++ src/main/java/com/flakesoup/zipkin/FlakeSoupZipkinApplication.java
@@ -0,0 +1,25 @@
+package com.flakesoup.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.Bean;
+import zipkin.storage.mysql.MySQLStorage;
+import zipkin2.server.internal.EnableZipkinServer;
+
+import javax.sql.DataSource;
+
+/**
+ * 服务链路追踪
+ */
+@EnableZipkinServer
+@SpringCloudApplication
+public class FlakeSoupZipkinApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FlakeSoupZipkinApplication.class, args);
+	}
+
+	@Bean
+	public MySQLStorage mySQLStorage(DataSource datasource) {
+		return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
+	}
+}
